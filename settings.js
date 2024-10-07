802b5f315f4fba537f91749cc10135be9fca31b8
Ayolite2216 <ayomideanthony746@gmail.com>
2024-10-07 21:09:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,24 +6,24 @@ const chalk = require('chalk')
 global.sessionid ='put your session id here'
 
 //owmner v card
-global.ytname = "YT: ELITE-PRO-V2" //ur yt chanel name
-global.socialm = "GitHub: Elite-MD" //ur github or insta name
+global.ytname = "YT: Ayolite FC" //ur yt chanel name
+global.socialm = "GitHub: AYOLITE FC" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'Ayolite FC' //ur bot name
+global.ownernumber = '2347036921983' //ur owner number
+global.ownername = 'Ayolite FC' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
+global.wm = "AYOLITE_FC"
 global.botscript = 'https://github.com/Elite-MD/ELITE-PRO-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2"
-global.creator = "2348109263390@s.whatsapp.net"
+global.creator = "2347036921983@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["2347036921983"] // Premium User
 global.hituet = 0
 
 //bot sett
